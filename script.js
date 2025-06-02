3518142169889311281f061bbc494637eba3da3a
lamxbang <tegar.hrlm@gmail.com>
2025-06-02 20:30:40 +0700

    first landing

---
+++ script.js
@@ -0,0 +1,485 @@
+// --- 1. Data embedded directly in JS ---
+const database = [
+  {
+    nisn: "0107526372",
+    nama: "ABELIYA INDAH SAFITRI",
+    nilai: "86.93",
+    ket: "LULUS",
+    catatan: "hvjh",
+  },
+  {
+    nisn: "0085016194",
+    nama: "ADHITYA PRATAMA",
+    nilai: "87.98",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3094319144",
+    nama: "AHMAD RENDY SAPUTRA",
+    nilai: "86.63",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3105632674",
+    nama: "AHMAD REVANSYAH",
+    nilai: "88.59",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0098952410",
+    nama: "AHMAT RISKI",
+    nilai: "84.41",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3092416971",
+    nama: "ALFIA KARIMATUN NISA",
+    nilai: "86.79",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3103806979",
+    nama: "APPRILIA RAHMA NUR HIDAYAH",
+    nilai: "84.74",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3101421898",
+    nama: "AQSA ZUNIAR YUSIANVIRA",
+    nilai: "95.91",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3109669973",
+    nama: "AYU ANGGRAENI PRATIWI",
+    nilai: "90.3",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0086009460",
+    nama: "DEWI INTAN ASTI AGUSTIN",
+    nilai: "88.19",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0109658689",
+    nama: "FIKA HIDAYATUS SHOLIHAH",
+    nilai: "85.47",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0105293657",
+    nama: "GALUH RETNO PARAMITHA",
+    nilai: "89.63",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0107969183",
+    nama: "JULIA CAHAYA MAHARANI",
+    nilai: "91.78",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0095479763",
+    nama: "KHAVID MAULANA ASRAFFI",
+    nilai: "86.42",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0099577034",
+    nama: "M. CIKO ARDIANSYAH",
+    nilai: "85.27",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3106555517",
+    nama: "M. RIFQI FUAD HABIBI",
+    nilai: "86.28",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3108307320",
+    nama: "MOH. NAZRIL HISAM",
+    nilai: "85.04",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3099136470",
+    nama: "MOHAMAD SEPTIAN ALDI PRATAMA",
+    nilai: "85.36",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0104071072",
+    nama: "MUHAMMAD FADHIL",
+    nilai: "84.24",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3100322981",
+    nama: "MUHAMMAD GILANG FABRILIAN",
+    nilai: "84.53",
+    ket: "BELUM LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3105266950",
+    nama: "MUHAMMAD NASRUL RAFA",
+    nilai: "84.65",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0101917852",
+    nama: "NINA ANGGUN PUTRI VANESA",
+    nilai: "88.89",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0109379861",
+    nama: "RAJWA RAHMATIKA ABIYAKSA",
+    nilai: "90.08",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0099244014",
+    nama: "RIZKI DWI PRASETIO",
+    nilai: "84.65",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3102208033",
+    nama: "SAEILA PUTRI APRILIA",
+    nilai: "87.28",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0098027075",
+    nama: "SATRIA AKBAR PRATAMA",
+    nilai: "83.69",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0105886416",
+    nama: "SITI NUR JANNAH",
+    nilai: "86.44",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0086930452",
+    nama: "SOFAHTUL MARWAH AZZAHRA",
+    nilai: "85.44",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0094871466",
+    nama: "VIVIN EKA WIDYA ARIANTI",
+    nilai: "85.92",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0104058431",
+    nama: "ZAHRANI MAULIDINA",
+    nilai: "87.02",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3108511057",
+    nama: "AHMAD RAFFI ADNAN HASAN",
+    nilai: "87",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0093705020",
+    nama: "ALIZE ZIVANNA LETISHA",
+    nilai: "91",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0092756803",
+    nama: "AMANDA ZAHIRO AINUHA",
+    nilai: "87",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0106527940",
+    nama: "ANGELINA SALWA",
+    nilai: "87",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0103593863",
+    nama: "APRILIA RAHMAWATI",
+    nilai: "90",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0105751603",
+    nama: "AULIA ROHMAH",
+    nilai: "89",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0099740261",
+    nama: "AURA RIYADUL JANNAH",
+    nilai: "89",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0099095598",
+    nama: "BAGAS PRATAMA",
+    nilai: "92",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3104278005",
+    nama: "BAKTI SETYO AJI",
+    nilai: "84",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0097353271",
+    nama: "DAMAR RADITYA SURANATA",
+    nilai: "84",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3092610243",
+    nama: "DICKY PRATAMA",
+    nilai: "84",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0096541294",
+    nama: "FAISOL ADI PRATAMA",
+    nilai: "83",
+    ket: "LULUS.",
+    catatan:
+      "Segera laksanakan ujian praktik Bahasa Inggris dan Bahasa Indonesia!",
+  },
+  {
+    nisn: "0099599967",
+    nama: "JESSICA ALVIONA LISANDRA",
+    nilai: "90",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3093977512",
+    nama: "KARIMATUN NAJWA",
+    nilai: "88",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0095362670",
+    nama: "KHAULI' LAIYYINAH",
+    nilai: "85",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3099325757",
+    nama: "LAILATUL FITRIANI",
+    nilai: "87",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3093056604",
+    nama: "MENTARI RONA CAHAYA NINGRUM",
+    nilai: "88",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0107789016",
+    nama: "MOCH.ZADA FATCHUL HIDAYAT",
+    nilai: "88",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0102393027",
+    nama: "MOH. AZZAM ALI MUZAKKI",
+    nilai: "85",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0097477958",
+    nama: "RAHMA WAHYU PAMBUDI",
+    nilai: "84",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0109285431",
+    nama: "RANI FEBRIANTI",
+    nilai: "87",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3094634493",
+    nama: "RIZKY ADITYA SAPUTRA",
+    nilai: "85",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "3090336376",
+    nama: "SAFA ANDIANI",
+    nilai: "89",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0102692004",
+    nama: "SAFAUL PUDJI MUZAFI",
+    nilai: "93",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0099055286",
+    nama: "SATRIA HARKAYA",
+    nilai: "85",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0093900786",
+    nama: "SATRIA RAMADHANU AL AYUBI",
+    nilai: "85",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0086930452",
+    nama: "VIONA DINDA LESTARI",
+    nilai: "86",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0091758927",
+    nama: "VISA ZAKIYATUL MISKIYA",
+    nilai: "87",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0104058431",
+    nama: "ZAHROTUN NURROKHIMA",
+    nilai: "93",
+    ket: "LULUS",
+    catatan: "",
+  },
+  {
+    nisn: "0097886460",
+    nama: "ZAZKIYA ALYA MEILANI",
+    nilai: "88",
+    ket: "LULUS",
+    catatan: "",
+  },
+];
+
+// --- 2. Highlight specific conditions ---
+function highlightKet(ket) {
+  const lower = ket.toLowerCase();
+  if (lower.includes("bersyarat") || lower.includes("catatan")) {
+    return `<span class="warning">${ket}</span>`;
+  }
+  return ket;
+}
+
+// --- 3. Show result on result.html ---
+function showResult() {
+  const nisnSearched = localStorage.getItem("searchedNISN");
+  const resultDiv = document.getElementById("result");
+
+  const result = database.find((item) => item.nisn === nisnSearched);
+
+  if (result) {
+    resultDiv.innerHTML = `
+  <div class="data-row"><span class="label">NISN</span><span class="value">${
+    result.nisn
+  }</span></div>
+  <div class="data-row"><span class="label">Nama</span><span class="value">${
+    result.nama
+  }</span></div>
+  <div class="data-row"><span class="label">Nilai</span><span class="value">${
+    result.nilai
+  }</span></div>
+  <div class="data-row"><span class="label">Keterangan</span><span class="value">${highlightKet(
+    result.ket
+  )}</span></div>
+  ${
+    result.ket.toLowerCase().includes(".") && result.catatan
+      ? `<div class="catatan-only">⚠️ ${result.catatan}</div>`
+      : ""
+  }
+`;
+  } else {
+    resultDiv.innerHTML = `<p class="warning">Data tidak ditemukan untuk NISN: ${nisnSearched}</p>`;
+  }
+}
+
+//keterangan
+function highlightKet(ket) {
+  const lower = ket.toLowerCase();
+
+  if (lower.includes("belum lulus")) {
+    return `<span class="warning">⚠️ ${ket}</span>`;
+  }
+
+  if (lower === "lulus") {
+    return `<span class="success">✔️ ${ket}</span>`;
+  }
+
+  return ket;
+}
+
+// Only run if on result.html
+if (document.getElementById("result")) {
+  showResult();
+}
